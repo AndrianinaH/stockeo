933fc78372dc6309f274c81d58f292afa0641a84
AndrianinaH <rakotoandrianina@gmail.com>
2024-05-20 10:06:35 +0300

    clean and remove all prisma dependances

--- BACKEND/configs/env.ts
+++ BACKEND/configs/env.ts
@@ -3,10 +3,6 @@ await configEnv({ export: true });
 
 export const config = {
   db: {
-    name: Deno.env.get("DB_NAME"),
-    host: Deno.env.get("DB_HOST"),
-    username: Deno.env.get("DB_USERNAME"),
-    password: Deno.env.get("DB_PASSWORD"),
     url: Deno.env.get("DATABASE_URL"),
   },
 };
