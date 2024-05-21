93b628c287a1e411631029d53876cde22d3e8224
AndrianinaH <rakotoandrianina@gmail.com>
2024-05-21 14:34:14 +0300

    clear all deps

--- BACKEND/routes.ts
+++ BACKEND/routes.ts
@@ -1,6 +1,11 @@
 import { Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 
 export const router = new Router().get("/", async ({ response }) => {
+  try {
+  } catch (error) {
+    console.log("🚀 ~ router ~ error:", error);
+  }
+
   response.body = "Stockeo API build with deno !!";
 });
 
