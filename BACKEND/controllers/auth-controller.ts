b648217ac898d2501887d7e51b6313c76df4981f
AndrianinaH <rakotoandrianina@gmail.com>
2024-06-04 10:41:12 +0300

    login controller ok

--- BACKEND/controllers/auth-controller.ts
+++ BACKEND/controllers/auth-controller.ts
@@ -3,10 +3,15 @@ import { AuthService } from "../services/auth.service.ts";
 
 export const AuthController = {
   login: async ({ request, response }: Context) => {
-    const body = await request.body().value;
-    const accessToken = await AuthService.login(body);
-    response.body = {
-      accessToken,
-    };
+    try {
+      const body = await request.body().value;
+      const accessToken = await AuthService.login(body);
+      response.body = {
+        accessToken,
+      };
+    } catch (error) {
+      response.status = 404;
+      response.body = error.message;
+    }
   },
 };
