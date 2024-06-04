4a169e89e16e7bed74123f4c01495d8715061e8a
AndrianinaH <rakotoandrianina@gmail.com>
2024-06-04 10:56:55 +0300

    add logger package for better log

--- BACKEND/controllers/auth-controller.ts
+++ BACKEND/controllers/auth-controller.ts
@@ -1,15 +1,18 @@
 import { Context } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 import { AuthService } from "../services/auth.service.ts";
+import { logger } from "../utils/logger.ts";
 
 export const AuthController = {
   login: async ({ request, response }: Context) => {
     try {
       const body = await request.body().value;
       const accessToken = await AuthService.login(body);
+      logger.info("login success", { user: body.email });
       response.body = {
         accessToken,
       };
     } catch (error) {
+      logger.error("login failed", { error: error.message });
       response.status = 404;
       response.body = error.message;
     }
